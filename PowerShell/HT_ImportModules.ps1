49d43d958402b71557193aa9cf97f5c1dc164dd8
Anders Spælling <spaelling@gmail.com>
2019-01-23 12:24:37 +0100

    added note on module load times

--- PowerShell/HT_ImportModules.ps1
+++ PowerShell/HT_ImportModules.ps1
@@ -8,6 +8,6 @@ $env:PSModulePath += ";$ModulePath"
 # We can list available modules starting with Az
 #Get-Module -ListAvailable Az*
 
-# Import specific modules from Az
+# Import specific modules from Az - these take around 3 seconds on a consumption plan, and 2 on a production S1
 Import-Module Az.Accounts
 Import-Module Az.Resources
